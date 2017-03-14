b845de4c363aa3df7820f049d36aae210969d1d2
ka <ka.kaosf@gmail.com>
2017-03-15 02:08:23 +0900

    Modify the default history_size to 30000

--- lib/earthquake/core.rb
+++ lib/earthquake/core.rb
@@ -67,7 +67,7 @@ module Earthquake
         api_version:     '1.1',
         secure:          true,
         output_interval: 1,
-        history_size:    1000,
+        history_size:    30000,
         api:             { :host => 'userstream.twitter.com', :path => '/1.1/user.json', :ssl => true },
         confirm_type:    :y,
         expand_url:      false,
